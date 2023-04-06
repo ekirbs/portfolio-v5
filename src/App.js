e253566c483bd6644f9a299d45cb04a3f968f0db
Eric K <erickirberger@gmail.com>
2023-04-06 16:45:51 -0400

    base build and migration

--- src/App.js
+++ src/App.js
@@ -1,41 +1,15 @@
-import { useState } from 'react';
-import { Navbar, Footer } from "./components";
-// import { Home, Portfolio, Resume, LoFi, Weather, Contact } from './pages'
+import React from 'react';
+import { Navbar, Home, Contact } from "./components";
 
-import "./assets/css/style.css";
+import "./assets/css/baseStyle.css";
 
 export default function App() {
-  const [currentPage, setCurrentPage] = useState('Home');
-
-  const renderPage = () => {
-    if (currentPage === 'Home') {
-      return <Home />;
-    }
-    if (currentPage === 'Portfolio') {
-      return <Portfolio />;
-    }
-    if (currentPage === 'Resume') {
-      return <Resume />;
-    }
-    if (currentPage === 'LoFi') {
-      return <LoFi />;
-    }
-    if (currentPage === 'Weather') {
-      return <Weather />;
-    }
-    if (currentPage === 'Map') {
-      return <Map />;
-    }
-    return <Contact />;
-  };
-
-  const handlePageChange = (page) => setCurrentPage(page);
-
+ 
   return (
     <>
-      <Navbar currentPage={currentPage} handlePageChange={handlePageChange} />
-      {renderPage()}
-      <Footer />
+      <Navbar />
+      <Home />
+      <Contact />
     </>
   );
 }
---
+++ src/assets/css/index.js
@@ -0,0 +1,4 @@
+export { default as globalStyle } from "./globalStyle";
+export { default as navStyle } from "./navStyle";
+export { default as mainStyle } from "./mainStyle";
+export { default as contactStyle } from "./contactStyle";
