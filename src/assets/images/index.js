c71c8388ce725ec9e51397a63b1896dec7b0c278
Eric K <erickirberger@gmail.com>
2023-04-26 11:40:31 -0400

    deploy fix

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,11 +1,8 @@
-export { default as thinkingRobot } from "./thinking-robot.jpg";
 export { default as blackSpace } from "./black-space.png";
-export { default as fireworksRoad } from "./fireworks-road.jpg";
 export { default as instagramLogo } from "./instagram-logo.png";
 export { default as githubLogo } from "./github-logo.jpg";
 export { default as linkedinLogo } from "./linkedin-logo.jpg";
 export { default as robotHead } from "./robot-head.png";
-export { default as treeRoad } from "./tree-road.jpg";
 export { default as profilePhoto } from "./profile-photo.jpg";
 export { default as puddleShineGif } from "./puddle-shine.gif";
 export { default as puddleStoresGif } from "./puddle-stores.gif";
